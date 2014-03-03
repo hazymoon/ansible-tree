7c1c27fd03f22f33d31dc1d929df31333d2bfcd9
Kakeru Sugibuchi <fine.despair@gmail.com>
2014-03-03 11:24:32 +0900

    テストの追加

--- spec/default/base_spec.rb
+++ spec/default/base_spec.rb
@@ -1,7 +1,41 @@
 # -*- coding: ascii-8bit -*-
 require 'spec_helper'
+describe yumrepo('epel') do
+  it { should exist }
+end
+
+describe yumrepo('epel') do
+  it { should be_enabled }
+end
+
+describe package 'yum-plugin-fastestmirror' do
+  it {should be_installed}
+end
 
 describe command('/usr/bin/yum grouplist "Development tools"') do
   it {should return_stdout(/Installed Groups/)}
 end
 
+describe package 'mlocate' do
+  it {should be_installed}
+end
+
+describe package 'telnet' do
+  it {should be_installed}
+end
+
+describe package 'bind-utils' do
+  it {should be_installed}
+end
+
+describe package 'vim-enhanced' do
+  it {should be_installed}
+end
+
+describe package 'zsh' do
+  it {should be_installed}
+end
+
+describe package 'tmux' do
+  it {should be_installed}
+end
